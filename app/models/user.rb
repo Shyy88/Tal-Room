b6f0198eb15ba11d4ce12beccf7fd8aa187ca264
Aishy A <114661886+Shyy88@users.noreply.github.com>
2023-01-09 08:03:03 +0700

    Add files via upload

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+
+    validates_uniqueness_of :username
+  
+    def self.generate
+      adjectives = ['Attractive', 'Charming', 'Wonderful', 'Effective', 'Flying', 'Gentle']
+      nouns = ['Highway', 'Intern', 'Sherlock', 'King', 'Sensei']
+      number = rand.to_s[2..4]
+      username = "#{adjectives.sample}-#{nouns.sample}-#{number}"
+      create(username: username)
+    end
+  
+  end
